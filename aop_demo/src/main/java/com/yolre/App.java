12b03355a3d0df2b5e38a27039244e08591773c6
yolre <291384100@qq.com>
2022-01-03 23:07:58 +0800

    新增输出I'm sad

--- aop_demo/src/main/java/com/yolre/App.java
+++ aop_demo/src/main/java/com/yolre/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("I'm A");
+        System.out.println("I'm sad");
     }
 }
