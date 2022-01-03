abbf7dde4074bbe354f60fcb5513980dfaf112c3
yolre <291384100@qq.com>
2022-01-03 23:07:58 +0800

    合并aaa的输出A和输出sad

--- aop_demo/src/main/java/com/yolre/App.java
+++ aop_demo/src/main/java/com/yolre/App.java
@@ -7,5 +7,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("I'm master");//我是master
+        System.out.println("I'm A");
+        System.out.println("I'm sad");
     }
 }
